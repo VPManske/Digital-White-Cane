8be638aa855957c3aca2074b9097aed998b61704
George Albercook <albercook@gmail.com>
2017-09-03 12:13:54 -0400

    Add files via upload
    Read beginning of .ino file

---
+++ three programs that kinda work/first/TeensyForDWCfolder/Wire.h
@@ -0,0 +1,16 @@
+//
+//  wire.h
+//  
+//
+//  Created by George Albercook on 9/2/17.
+//
+//
+
+#ifndef _wire_h
+#define _wire_h
+
+// Include any system framework and library headers here that should be included in all compilation units.
+// You will also need to set the Prefix Header build setting of one or more of your targets to reference this file.
+#include <i2c_t3.h>
+
+#endif
