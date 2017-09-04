ee3033aad7d982e57b1f4ffb0bda10ff719e151e
lthierbach <lthierbach@users.noreply.github.com>
2017-09-04 00:30:31 -0400

    Added ablity to read in measurement for up to five sensors.

--- three programs that kinda work/first/TeensyForDWC/TeensyForDWC.ino
+++ three programs that kinda work/first/TeensyForDWC/TeensyForDWC.ino
@@ -5,38 +5,72 @@
 
 #define DEBUG true
 
-int tofaddress = 1;
+int num_of_sensors = 1;
 
-Adafruit_VL53L0X tof;
+int xshut[5] = {0,3,5,7,9};
+int gpio1[5] = {3,6,8,10,12};
+int tofaddress[5] = {0x1,0x2,0x3,0x4,0x5};
 
-void setup() {
-  // put your setup code here, to run once:
+Adafruit_VL53L0X tof[5];
+
+void setup() 
+{
 
   Serial.begin(115200);
 
   // wait until serial port opens for native USB devices
-  while (! Serial) {
-    delay(1);
-    }
-    Serial.println("step one");
- for (int i =0;i<2;i++)
+  while (! Serial) 
   {
-    tof = Adafruit_VL53L0X();
-    Serial.println("step two"); 
-      
-    if(!tof.begin(tofaddress, DEBUG))
+    delay(1);
+  }
+
+  Serial.println("step one");
+
+  for (int i = 0; i < num_of_sensors; i++)
+  {
+    pinMode(xshut[i], OUTPUT);
+    digitalWrite(xshut[i], HIGH);
+    pinMode(gpio1[i], INPUT);
+  }
+
+  Serial.println("step two"); 
+  for (int i =0;i<num_of_sensors;i++)
+  {
+    tof[i] = Adafruit_VL53L0X();
+
+    digitalWrite(xshut[i], LOW);
+    delay(1);
+
+    if(!tof[i].begin(tofaddress[i], DEBUG))
     {
       Serial.println(F("tof error"));
       while(1);
     }
-    else Serial.println("no error");
     delay(1);
   }
+  Serial.println("no error\n Setup finished\n");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
- 
+
+void loop() 
+{
+  
+  VL53L0X_RangingMeasurementData_t measure[5];
+
+  for(int i=0;i<num_of_sensors;i++)
+  {
+    tof[i].rangingTest(&measure[i], DEBUG);
+    if (measure[i].RangeStatus !=4) 
+    {
+      Serial.print("Sensor ");
+      Serial.print(i);
+      Serial.print(" Distance (mm): ");
+      Serial.println(measure[i].RangeMilliMeter);
+    }
+    else
+      Serial.println(" out of range ");
+    delay(100);
+  } 
 
 
 }
