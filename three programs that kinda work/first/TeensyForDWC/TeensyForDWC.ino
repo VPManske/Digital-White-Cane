2d55deedcfd91ed6b32e3bb41798aafe7eab5808
Paul Haas <hamjudo@gmail.com>
2017-09-11 22:12:39 -0400

    Initialize the noise makers once. Delay sending new commands until we're sure the last one finished, because the thing locks up sometimes. Also wrote code to look for lockups and reset everything. This didn't work nearly as well as preventing the lockups in the first place.

--- three programs that kinda work/first/TeensyForDWC/TeensyForDWC.ino
+++ three programs that kinda work/first/TeensyForDWC/TeensyForDWC.ino
@@ -15,6 +15,10 @@ int num_of_sensors = 5;
 uint8_t leds[5] = {21, 20, 91, 18, 15};
 uint8_t xshut[5] = {2, 3,  5, 7, 9};
 uint8_t i2cAddr[5] = {2, 3,  5, 7, 9};
+uint8_t lastEffect[5] = {0, 0, 0, 0, 0};
+uint32_t lastEffectStart[5] = {0, 0, 0, 0, 0};
+uint32_t lastEffectEnd[5] = {0, 0, 0, 0, 0};
+
 #define TCAADDR 0x71 // Why didn't we leave it at 0x70???
 
 void tcaselect(uint8_t i) {
@@ -30,22 +34,35 @@ void tcaselect(uint8_t i) {
   }
   Wire.endTransmission();
 }
-void init_tofs() {
+void init_haptic(int hapticId) {
+  tcaselect(hapticId);
+  drv.begin();
+
+  drv.selectLibrary(1);
+
+  // I2C trigger by sending 'go' command
+  // default, internal trigger when sending GO command
+  drv.setMode(DRV2605_MODE_INTTRIG);
+}
+void init_tof(int tofId) {
   VL53L0X_RangingMeasurementData_t datum;
   VL53L0X_Error errCode;
+  digitalWrite(xshut[tofId], HIGH);
+  delay(2);
+  Serial.print(": ");
+  tofs[tofId].begin(i2cAddr[tofId], false, I2C_PINS_22_23);
+  errCode = tofs[tofId].getSingleRangingMeasurement(&datum,  false );
+  Serial.print( datum.RangeMilliMeter );
+}
+void init_tofs_haptic() { //
+
   long startTime = millis();
   Serial.print("Initialising tof sensors. ");
   for (int i = 0; i < num_of_sensors; i ++ ) {
     Serial.print(i);
-    digitalWrite(xshut[i], HIGH);
-    delay(2);
-    Serial.print(": ");
-    tofs[i].begin(i2cAddr[i], false, I2C_PINS_22_23);
-    errCode = tofs[i].getSingleRangingMeasurement(&datum,  false );
-
-
-    Serial.print( datum.RangeMilliMeter );
+    init_tof(i);
     Serial.print("  ");
+    init_haptic(i);
   }
   Serial.println(millis() - startTime);
 }
@@ -75,12 +92,32 @@ void setup() {
   Serial.println("step one");
 
   Wire.begin();// Default i2c bus kludged in the library to be on pins 16 and 17
+
   Wire1.begin();
-  init_tofs();
+//  const uint32_t i2cDefaultTimeout = 2100000; // (2.1 seconds).(Has to be longer than the longest haptic waveform)
+//  Wire.setDefaultTimeout(i2cDefaultTimeout);
+//  Wire1.setDefaultTimeout(i2cDefaultTimeout);
+  init_tofs_haptic();
 
 
 }
-
+void checkHealth() {
+  if ( Wire.getError() >= 2 || Wire1.getError() >= 2 ) {
+    Serial.println("Start BUS RESET, THIS IS NOT SUPPOSED TO HAPPEN (except during testing");
+    Wire.resetBus();
+    Wire1.resetBus();
+    setup();
+    Serial.println("End BUS RESET, THIS IS NOT SUPPOSED TO HAPPEN (except during testing");
+  }
+}
+void waitUntil(uint32_t goal) {
+// This needs to deal with the clock wrapping.
+  uint32_t now=millis();
+  while( now < goal ) {
+    delay(10);
+    now = millis();
+  }
+}
 void loop() {
   VL53L0X_RangingMeasurementData_t datum;
   VL53L0X_Error errCode;
@@ -96,14 +133,7 @@ void loop() {
     Serial.print( datum.RangeMilliMeter );
     Serial.print("  ");
     if ( datum.RangeMilliMeter < 1000 ) {
-      tcaselect(i);
-      drv.begin();
 
-      drv.selectLibrary(1);
-
-      // I2C trigger by sending 'go' command
-      // default, internal trigger when sending GO command
-      drv.setMode(DRV2605_MODE_INTTRIG);
       int effect = 0;
       if ( datum.RangeMilliMeter < 200 ) {
         effect = 47; // Buzz 1 - 100%
@@ -113,15 +143,46 @@ void loop() {
         effect = 49;  // Buzz 3 - 60%
       } else if ( datum.RangeMilliMeter < 800 ) {
         effect = 50;  // Buzz 4 - 40%
-       } else if ( datum.RangeMilliMeter < 1000 ) {
+      } else if ( datum.RangeMilliMeter < 1000 ) {
         effect = 50;  // Buzz 5 - 20%
-         }
-      drv.setWaveform(0, effect);
-      drv.setWaveform(1, 0);
-      drv.go();
+      }
+      uint32_t now = millis();
+      // Don't ask to start playing a new waveform until the last one is finished.
+      // If the current waveform will finish in 100 ms or less, just wait.
+      if ( now < lastEffectEnd[i] &&  now > (lastEffectEnd[i]-100) ) {
+        Serial.print("#");
+        waitUntil(lastEffectEnd[i]);
+        now =millis();
+      }
+      // If the current waveform is done, start the next one, otherwise
+      // skip that one and get it on the next cycle.
+      // I am assuming I will be able to speed up cycles so the skip function is useful.
+      // As of this version, the loop tends to take 115 or more milliseconds. The
+      // waveform takes around 210 milliseconds. So we never skip. (other waveforms take longer).
+      if ( now >= lastEffectEnd[i] ) {
+        tcaselect(i);
+        /*  if ( lastEffect[i] != effect ) {
+            tcaselect(i);
+            drv.stop();
+            lastEffect[i] = effect;
+            drv.setWaveform(0, effect);
+            drv.setWaveform(1, 0);
+            drv.go();
+          } */
+  
+        drv.stop();
+        Serial.print("* ");
+        lastEffectStart[i] = now;
+        lastEffectEnd[i] = now + 210; // With 200 milliseconds, sometimes it got confused.
+        lastEffect[i] = effect;
+        drv.setWaveform(0, effect);
+        drv.setWaveform(1, 0);
+        drv.go();
+      }
+
     }
   }
   Serial.println(millis() - startTime);
-
+  // checkHealth();
 }
 
